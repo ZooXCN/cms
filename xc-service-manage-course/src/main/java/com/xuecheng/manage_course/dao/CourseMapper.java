845966986a0bd3e4b206102f54229780ed545a93
tomzhang <1451466174@qq.com>
2019-04-22 21:30:54 +0800

    新增了课程计划管理，对TeachPlan数据表进行left join关联查询，实现树型目录结构目录 (parentId 和 Id 父级和子级目录)

--- xc-service-manage-course/src/main/java/com/xuecheng/manage_course/dao/CourseMapper.java
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/dao/CourseMapper.java
@@ -1,6 +1,7 @@
 package com.xuecheng.manage_course.dao;
 
 import com.xuecheng.framework.domain.course.CourseBase;
+import com.xuecheng.framework.domain.course.ext.TeachplanNode;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
@@ -8,5 +9,8 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface CourseMapper {
+
    CourseBase findCourseBaseById(String id);
+
+
 }
